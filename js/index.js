fa19642c85d7f3439df0c02201ec98ae81f66291
Santiago <SNT1703@protonmail.com>
2023-05-24 15:52:55 -0500

    funcion copiar texto agregada

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // Encriptado
+const textContainer = document.getElementById("container")
+textoArea = document.getElementById("texto");
+btnEncriptar = document.getElementById("Encri")
+btnDesencriptar = document.getElementById("Desencri")
+recordatorio = document.getElementById("recordatorio")
+ventana = document.getElementById("alert")
+equis = document.getElementById("equis")
+tapar = document.getElementById("tapar")
+buttonCopy=document.getElementById("copiar")
 
 function Encriptar(texto) {
+  recordatorio.style.fontSize="25px"
+  buttonCopy.style.opacity="1"
 
     return texto.replace(/e/ig, "enter")
         .replace(/i/ig, "imes")
@@ -10,25 +21,18 @@ function Encriptar(texto) {
 
 }
 
-const textContainer = document.getElementById("container")
-textoArea = document.getElementById("texto");
-btnEncriptar = document.getElementById("Encri")
-btnDesencriptar = document.getElementById("Desencri")
-recordatorio = document.getElementById("recordatorio")
-ventana = document.getElementById("alert")
-equis = document.getElementById("equis")
 
 equis.addEventListener('click', function() {
   // Obtener la referencia a la ventana
   var window = document.querySelector('#alert');
-  
+
   // Desvanecer la ventana utilizando CSS
   window.style.opacity = '0';
 
   // Opcional: eliminar la ventana del DOM después de un retraso
   setTimeout(function() {
     window.style.display = 'none';
-  }, 1000); // Desaparece después de 1 segundo (1000 milisegundos)
+  },); // Desaparece después de 1 segundo (1000 milisegundos)
 });
 
 function detectarMayuscula() {
@@ -40,11 +44,11 @@ function detectarMayuscula() {
       // change color re if the condition 
       recordatorio.style.color = 'red';
       recordatorio.style.fontSize = "30px";
-      ventana.style.opacity= "1"
-      
+      ventana.style.opacity= '1';
+      ventana.style.transform= "translate(-50%, -50%)";
     } else {
       // change color of default
-      recordatorio.style.color = 'black';
+      recordatorio.style.color = 'gray';
     }
   }
 
@@ -73,3 +77,21 @@ btnDesencriptar.onclick = function (event) {
     textContainer.textContent = Desencriptar(textoArea.value)
 }
 
+// copy clipboard
+function copiarTexto() {
+  const contenedor = document.getElementById('container');
+
+  const elementoTemporal = document.createElement('textarea');
+  elementoTemporal.value = contenedor.innerText;
+
+  document.body.appendChild(elementoTemporal);
+
+  elementoTemporal.select();
+
+  document.execCommand('copy');
+
+  document.body.removeChild(elementoTemporal);
+}
+
+// Agrega un evento de clic al botón
+buttonCopy.addEventListener('click', copiarTexto);
